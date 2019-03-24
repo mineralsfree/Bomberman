bea4bd79822e2a53e71ba593fc5c96e10e347eee
Nikita Pilinka <mineralsfree@gmail.com>
2019-03-24 17:27:29 +0300

    Updates

---
+++ precache-manifest.909dd14a582576b85a7264f1b68526e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "68eac9705ccc0d705271",
+    "url": "/Bomberman/static/css/main.dc15e4c8.chunk.css"
+  },
+  {
+    "revision": "68eac9705ccc0d705271",
+    "url": "/Bomberman/static/js/main.68eac970.chunk.js"
+  },
+  {
+    "revision": "190bf62b19dbdf635153",
+    "url": "/Bomberman/static/js/runtime~main.190bf62b.js"
+  },
+  {
+    "revision": "e49762ef1f5fb6363ba0",
+    "url": "/Bomberman/static/js/2.e49762ef.chunk.js"
+  },
+  {
+    "revision": "cea44ec295e883944a2a0dcf2344c5c7",
+    "url": "/Bomberman/static/media/bomberman.cea44ec2.png"
+  },
+  {
+    "revision": "7dd2928da4e9f5677851e12783872529",
+    "url": "/Bomberman/index.html"
+  }
+];
